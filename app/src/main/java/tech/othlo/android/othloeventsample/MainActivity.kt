ed44426a601248c89c6e621774959a4429ad86f1
Yohei Murayama <muracchi@gmail.com>
2018-10-28 23:28:51 +0900

    ボタンの実装

--- app/src/main/java/tech/othlo/android/othloeventsample/MainActivity.kt
+++ app/src/main/java/tech/othlo/android/othloeventsample/MainActivity.kt
@@ -2,11 +2,19 @@ package tech.othlo.android.othloeventsample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.textView)
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener { _ ->
+            textView.text = "Yohei Murayama"
+        }
     }
 }
