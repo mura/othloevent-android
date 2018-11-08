6ba53e836117af5fe5e92afac2500ac778e7509b
Yohei Murayama <muracchi@gmail.com>
2018-11-08 21:40:49 +0900

    SharedPreferencesの実装

--- app/src/main/java/tech/othlo/android/othloeventsample/MainActivity.kt
+++ app/src/main/java/tech/othlo/android/othloeventsample/MainActivity.kt
@@ -1,22 +1,37 @@
 package tech.othlo.android.othloeventsample
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        const val PREF_NAME = "settings"
+    }
+
+    private lateinit var settings: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        settings = getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE)
+
         val textView = findViewById<TextView>(R.id.textView)
+        textView.text = settings.getString("text", "DEFAULT!")
+
         val button = findViewById<Button>(R.id.button)
         val editText = findViewById<EditText>(R.id.editText)
         button.setOnClickListener { _ ->
-            textView.text = editText.text
+            val inputText = editText.text.toString()
+            textView.text = inputText
+            settings.edit()
+                .putString("text", inputText)
+                .apply()
         }
     }
 }
