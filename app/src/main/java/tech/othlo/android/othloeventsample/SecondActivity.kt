de46c87c87a7a22f2080a8ec565949206b78be5b
Yohei Murayama <muracchi@gmail.com>
2018-11-10 16:20:23 +0900

    RecyclerViewとAdapterの連携

--- app/src/main/java/tech/othlo/android/othloeventsample/SecondActivity.kt
+++ app/src/main/java/tech/othlo/android/othloeventsample/SecondActivity.kt
@@ -1,8 +1,9 @@
 package tech.othlo.android.othloeventsample
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 
 class SecondActivity : AppCompatActivity() {
 
@@ -10,9 +11,14 @@ class SecondActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
 
-        val nameTextView = findViewById<TextView>(R.id.nameTextView)
-        val name: String? = intent.getStringExtra("name")
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.setHasFixedSize(true)
+        recyclerView.layoutManager = LinearLayoutManager(this)
 
-        nameTextView.text = name
+        val months = listOf("1月","2月","3月","4月","5月","6月",
+            "7月","8月","9月","10月","11月","12月")
+        //val months = (1..12).map { "${it}月" }
+        //val months = (2004..2018).flatMap { y -> (1..12).map { m -> "${y}年${m}月"} }
+        recyclerView.adapter = MonthRecycleViewAdapter(months)
     }
 }
